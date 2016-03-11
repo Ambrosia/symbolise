d90a5a2a34a52896a272ea1f3cef9586954107ca
Ambrosia <ambrosia@hana.re>
2016-03-11 19:52:49 +0000

    Add initial Elm app and some packages

---
+++ src/Main.elm
@@ -0,0 +1,48 @@
+module Main (..) where
+
+import Html exposing (..)
+import Effects exposing ( Effects, Never )
+import Task
+import StartApp
+
+-- Actions
+type Action =
+  NoOp
+
+-- Model
+type alias Model = {}
+
+initialModel : Model
+initialModel =
+  {}
+
+-- Update
+update : Action -> Model -> ( Model, Effects Action )
+update action model =
+  (model, Effects.none)
+
+-- View
+view address model =
+  div [] []
+
+-- StartApp
+init : ( Model, Effects Action )
+init =
+  ( initialModel, Effects.none )
+
+app : StartApp.App Model
+app =
+  StartApp.start
+            { init = init
+            , inputs = []
+            , update = update
+            , view = view
+            }
+
+main : Signal.Signal Html
+main =
+  app.html
+
+port runner : Signal ( Task.Task Never () )
+port runner =
+  app.tasks
